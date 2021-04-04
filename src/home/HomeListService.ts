9c941ec37d3f862978dcabbbc2f9421545c42a0c
Amós Batista <amos.silva@gmail.com>
2021-04-04 18:23:09 -0300

    feat: home list service

---
+++ src/home/HomeListService.ts
@@ -0,0 +1,60 @@
+export class HomeListService {
+    async Load () {
+        return await [
+            {
+                "title": "23:59",
+                "custom_excerpt": null,
+                "slug": "23-59",
+                "url": "https://amosbatista-blog.herokuapp.com/23-59/"
+            },
+            {
+                "title": "Théo aos 15",
+                "custom_excerpt": null,
+                "slug": "theo-aos-15",
+                "url": "https://amosbatista-blog.herokuapp.com/theo-aos-15/"
+            },
+            {
+                "title": "foi melhor assim",
+                "custom_excerpt": null,
+                "slug": "foi-melhor-assim",
+                "url": "https://amosbatista-blog.herokuapp.com/foi-melhor-assim/"
+            },
+            {
+                "title": "nú",
+                "custom_excerpt": null,
+                "slug": "nu",
+                "url": "https://amosbatista-blog.herokuapp.com/nu/"
+            },
+            {
+                "title": "Brancos Lucrando com Black Lives Matter",
+                "custom_excerpt": null,
+                "slug": "brancos-lucrando-com-a-blm",
+                "url": "https://amosbatista-blog.herokuapp.com/brancos-lucrando-com-a-blm/"
+            },
+            {
+                "title": "Primeiro Post e Metadados",
+                "custom_excerpt": null,
+                "slug": "primeiro-post-e-metadados",
+                "url": "https://amosbatista-blog.herokuapp.com/primeiro-post-e-metadados/"
+            },
+            {
+                "title": "mate a pira",
+                "custom_excerpt": null,
+                "slug": "mate-a-pira",
+                "url": "https://amosbatista-blog.herokuapp.com/mate-a-pira/"
+            },
+            {
+                "title": "teste",
+                "custom_excerpt": null,
+                "slug": "teste",
+                "url": "https://amosbatista-blog.herokuapp.com/teste/"
+            },
+            {
+                "title": "Terra Preta, Mairiporã",
+                "custom_excerpt": "Nos arredores de São Paulo, um lugar de tranquilidade.",
+                "slug": "terra-preta-mairipora",
+                "url": "https://amosbatista-blog.herokuapp.com/terra-preta-mairipora/"
+            }
+        ]
+    }
+}
