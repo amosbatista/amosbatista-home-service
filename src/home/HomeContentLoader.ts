8684d01c07ce4367c0005e09340414b078691a9d
Amós Batista <amos.silva@gmail.com>
2022-02-17 20:24:47 -0300

    fix(home): alterado formato de dados home

---
+++ src/home/HomeContentLoader.ts
@@ -0,0 +1,59 @@
+import MapTranslatorService from '../map/MapTranslatorService';
+
+export default class HomeTopContentLoader {
+
+  homeContent: Array<any>;
+  client: any;
+
+  constructor (
+    redisClientFactory: Function, 
+    private mapTranslator = new MapTranslatorService
+  ) {
+    this.client = redisClientFactory();
+  }
+  
+  Load (): Promise<Array<any>> { 
+    return new Promise( (resolve, reject) => {
+      
+      Promise.all([
+        this.LoadHelper("home", "topContent"),
+        this.LoadHelper("home", "highLights"),
+      ])
+        .catch(err => reject(err))
+        .then(contents => {
+          resolve({
+            topContent: contents[0],
+            highLights: contents[1],
+          } as any);
+        });
+    });
+   }
+  
+
+  LoadHelper (root: string, address: string): Promise<Array<any>> {  
+
+    return new Promise( (resolve, reject) => {
+       const redisKey = this.mapTranslator.GetMapAddress(root, address);
+    
+      if(!redisKey) {
+        reject("No link found");
+        
+        return;
+      }
+      
+      this.client.get(
+        redisKey,
+        
+        (err, redisResponse) => {
+          this.client.quit();
+        
+          if(err) {
+            reject(err);
+          }
+          else{
+            resolve(redisResponse);
+          }
+      });
+    });
+  }
+}
--- src/home/HomeGenerateService.ts
+++ src/home/HomeGenerateService.ts
@@ -1,20 +1,41 @@
 import HomeContentInterface from "./HomeContentInterface";
+import MapTranslatorService from '../map/MapTranslatorService';
 
 class HomeGenerateService {
-  CHAVE_REDIS = "HOME:CONTENT";
   client: any;
 
-  constructor (redisClientFactory: Function) {
+  constructor (
+    redisClientFactory: Function,
+    private mapTranslator = new MapTranslatorService
+  ) {
     this.client = redisClientFactory();
   }
 
-  saveList(content: Array<HomeContentInterface>) {
-    this.client.del(this.CHAVE_REDIS);
+  saveList(content: Array<HomeContentInterface>, redisKey: string) {
+    this.client.del(redisKey);
     content.forEach( (post) => {
-      this.client.rpush(this.CHAVE_REDIS, JSON.stringify(post));
+      this.client.rpush(redisKey, JSON.stringify(post));
     });
 
     this.client.quit();
+  }
+  
+  SaveTopContent(content: Array<HomeContentInterface>) {
+    const redisKey = this.mapTranslator.GetMapAddress("home", "topContent");
+    
+    if(!redisKey) {
+      throw new Error("No link found");
+    }
+    this.saveList(content, redisKey);
+  }
+  
+  SaveHighlights(content: Array<HomeContentInterface>) {
+    const redisKey = this.mapTranslator.GetMapAddress("home", "highLights");
+    
+    if(!redisKey) {
+      throw new Error("No link found");
+    }
+    this.saveList(content, redisKey);
   }
 }
 
